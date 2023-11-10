51c1a47a402cd588086408842aa4d0e23ba97db4
Oldflat <ducksalt+github@gmail.com>
2023-11-09 21:48:41 -0500

    very beginnings

---
+++ hello.rs
@@ -0,0 +1,26 @@
+// This is a comment, and is ignored by the compiler.
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
+// shortcut.
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+
+    display_square_roots()
+}
+
+
+fn display_square_roots() {
+    
+}
+
+fn newton_rooter(f32: a, u16: n) {
+    return a / 2 + n / a
+}
